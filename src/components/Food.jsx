db12ee54256eba2aedbca28adf5bc4a0c9f06980
Sonali123676 <sonalisadana1@gmail.com>
2023-07-27 14:30:39 +0530

    Added Dropdown list

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -2,179 +2,222 @@ import React, { useState } from "react";
 import "./food.css";
 
 const FoodComponent = () => {
+  const [selectedDay, setSelectedDay] = useState("monday"); // Default selected day is Monday
+
+  const handleDayChange = (event) => {
+    setSelectedDay(event.target.value);
+  };
+
   const [selectedMeal, setSelectedMeal] = useState("breakfast");
-  const [selectedRiceOption, setSelectedRiceOption] = useState(null);
 
   const getMealDescription = () => {
     const mealDescriptions = {
-      breakfast: {
-        description: "Breakfast meals are here!!",
-        meals: [
-          {
-            name: "Poha",
-            description: "Poha is a popular Indian breakfast dish.",
-          },
-          {
-            name: "Poha",
-            description: "Poha is a popular Indian breakfast dish.",
-          },
-          {
-            name: "Poha",
-            description: "Poha is a popular Indian breakfast dish.",
-          },
-          // Add more breakfast options...
-        ],
+      monday: {
+        breakfast: {
+          meals: [
+            {
+              name: "Boiled Eggs & Omelette",
+            },
+            {
+              name: "Bread/Jam/Butter",
+            },
+            {
+              name: "Idly/Wada",
+            },
+            {
+              name: "Tea/Coffee/Milk",
+            },
+            {
+              name: "Sprouts/Corn Flakes",
+            },
+            // breakfast options...
+          ],
+        },
+        lunch: {
+          meals: [
+            {
+              name: "Plain Rice",
+            },
+            {
+              name: "Corn Pulao",
+            },
+            {
+              name: "Moong Dal",
+            },
+            {
+              name: "Mix Veg Curry",
+            },
+            {
+              name: "Egg Curry",
+            },
+            {
+              name: "Chapathi",
+            },
+            {
+              name: "Butter Milk",
+            },
+            {
+              name: "Semiya Kheer",
+            },
+            {
+              name: "Mango Pickle",
+            },
+            {
+              name: "Green Salad",
+            },
+            // lunch options...
+          ],
+        },
+        dinner: {
+          meals: [
+            {
+              name: "Plain Rice",
+            },
+            {
+              name: "Dal Thadka",
+            },
+            {
+              name: "Chole",
+            },
+            {
+              name: "Bhature",
+            },
+            {
+              name: "Cabbage Porial",
+            },
+            {
+              name: "Dal Kichidi",
+            },
+            {
+              name: "Veg Soup",
+            },
+            {
+              name: "Pulka",
+            },
+            {
+              name: "Veg Soup",
+            },
+            {
+              name: "Curd",
+            },
+            {
+              name: "Banana",
+            },
+            // dinner options...
+          ],
+        },
       },
-      lunch: {
-        description: "Lunch meals are here !!",
-        meals: [
-          {
-            name: "Plain Rice",
-          },
-          {
-            name: "Chapathi",
-          },
-          {
-            name: "Green Salad",
-          },
+      tuesday: {
+        breakfast: {
+          meals: [
+            {
+              name: "Boiled Eggs & Omelette",
+            },
+            {
+              name: "Bread/Jam/Butter/CornFlakes",
+            },
+            {
+              name: "Bonda/Poha/Sprouts",
+            },
+            {
+              name: "Tea/Coffee/Milk/BlackTea",
+            },
+            {
+              name: "Sprouts/Corn Flakes",
+            },
+            // breakfast options...
+          ],
+        },
+        lunch: {
+          meals: [
+            {
+              name: "Plain Rice",
+            },
+            {
+              name: "Curd Rice",
+            },
+            {
+              name: "Dal Pakhoni",
+            },
+            {
+              name: "Beerakaya Alu",
+            },
+            {
+              name: "Gobi Tomato Dum",
+            },
+            {
+              name: "Chapathi",
+            },
+            {
+              name: "Rasam",
+            },
+            {
+              name: "Vegetable Chatney",
+            },
+            {
+              name: "Green Salad",
+            },
+            // lunch options...
+          ],
+        },
+        dinner: {
+          meals: [
+            {
+              name: "Plain Rice",
+            },
+            {
+              name: "Palak Dal",
+            },
+            {
+              name: "Mushroom Capsicum",
+            },
+            {
+              name: "Bhendi Dum",
+            },
+            {
+              name: "Dal Kichidi",
+            },
+            {
+              name: "Chapathi",
+            },
 
-          {
-            name: "Rice Varieties",
-
-            options: [
-              {
-                name: "Corn Pulao",
-              },
-              {
-                name: "Curd Rice",
-              },
-              {
-                name: "Bagara Rice",
-              },
-              {
-                name: "Sambar Rice",
-              },
-              {
-                name: "Lemon Rice",
-              },
-            ],
-          },
-
-          {
-            name: "Veg Varieties",
-
-            options: [
-              {
-                name: "Moong Dal",
-              },
-              {
-                name: "Dal Thadka",
-              },
-              {
-                name: "lal Chana",
-              },
-              {
-                name: "Veg Biryani",
-              },
-            ],
-          },
-          {
-            name: "NonVeg Varieties",
-
-            options: [
-              {
-                name: "Egg Curry",
-              },
-              {
-                name: "Chicken Curry",
-              },
-              {
-                name: "Rogani Chicken",
-              },
-              {
-                name: "Beerakaya Curry",
-              },
-            ],
-          },
-          {
-            name: "Sides",
-
-            options: [
-              {
-                name: "Mango Pickle",
-              },
-              {
-                name: "Green Chatney",
-              },
-            ],
-          },
-          {
-            name: "Sweet Varieties",
-
-            options: [
-              {
-                name: "Semiya kheer",
-              },
-              {
-                name: "Suji Ka Halwa",
-              },
-              {
-                name: "Fruit Custurd",
-              },
-              {
-                name: "Jibe Gaja",
-              },
-            ],
-          },
-          {
-            name: "Drink Varieties",
-            description: "Choose from various Sweet options.",
-            options: [
-              {
-                name: "Butter Milk",
-              },
-              {
-                name: "Lemon juice",
-              },
-              {
-                name: "Mango Juice",
-              },
-              {
-                name: "Rasna",
-              },
-              {
-                name: "Roohafza",
-              },
-            ],
-          },
-
-          // Add more lunch options...
-        ],
+            {
+              name: "Banana",
+            },
+            // dinner options...
+          ],
+        },
       },
-      dinner: {
-        description: "Daily:Rice,Curd,Banana,Milk,Chapati",
-        meals: [
-          {
-            name: "Dal Rice",
-            description: "Dal served with rice.",
-          },
-          // Add more dinner options...
-        ],
-      },
+      // Add descriptions for other days...
     };
-
-    return mealDescriptions[selectedMeal];
+    return mealDescriptions[selectedDay][selectedMeal];
   };
 
   const mealDescription = getMealDescription();
 
-  const handleRiceOptionChange = (option) => {
-    setSelectedRiceOption(option);
-  };
-
   return (
     <section id="food" className="food">
       <h2 className="head">Food Menu</h2>
+      {/* Add the dropdown list */}
+      <div className="day-dropdown">
+        <label htmlFor="day">Select a day:</label>
+        <select
+          id="day"
+          name="day"
+          value={selectedDay}
+          onChange={handleDayChange}
+        >
+          <option value="monday">Monday</option>
+          <option value="tuesday">Tuesday</option>
+          <option value="wednesday">Wednesday</option>
+          <option value="thursday">Thursday</option>
+          <option value="friday">Friday</option>
+          <option value="saturday">Saturday</option>
+          <option value="sunday">Sunday</option>
+          {/* Add options for other days */}
+        </select>
+      </div>
+
       <div className="food__buttons">
         <button
           className={selectedMeal === "breakfast" ? "active" : ""}
@@ -213,33 +256,6 @@ const FoodComponent = () => {
               >
                 <div className="meal-card__info">
                   <h4>{meal.name}</h4>
-
-                  {selectedMeal === "lunch" &&
-                  (meal.name === "Rice Varieties" ||
-                    meal.name === "NonVeg Varieties" ||
-                    meal.name === "Veg Varieties" ||
-                    meal.name === "Drink Varieties" ||
-                    meal.name === "Sweet Varieties" ||
-                    meal.name === "Sides") ? (
-                    <div className="options-buttons">
-                      <h5 style={{ color: "GrayText", marginBottom: "5px" }}>
-                        Choose among them
-                      </h5>
-                      {meal.options.map((option, optionIndex) => (
-                        <button
-                          key={optionIndex}
-                          className={`option-button ${
-                            selectedRiceOption === option.name ? "active" : ""
-                          }`}
-                          onClick={() => handleRiceOptionChange(option.name)}
-                        >
-                          {option.name}
-                        </button>
-                      ))}
-                    </div>
-                  ) : (
-                    <p>{meal.description}</p>
-                  )}
                 </div>
               </div>
             ))}
